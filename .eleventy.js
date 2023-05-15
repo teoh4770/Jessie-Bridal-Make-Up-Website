cdb99a8ca30ad6ccca032a854dbc83d0d22c92b1
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-05-14 23:12:05 -0400

    ADD | 11ty and nunjucks setup done

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/app.js");
+  eleventyConfig.addPassthroughCopy("src/assets/img");
+
+  // eleventyConfig.addPassthroughCopy("./src/portfolio/portfolio.11tydata.js");
 
   return {
     dir: {
